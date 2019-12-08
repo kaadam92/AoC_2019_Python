7f9000572967322dbdde7791f53728a0b3879a7b
David Kiss <kiss.david92@gmail.com>
2019-12-08 10:20:56 +0100

    Day 4 start

---
+++ 4_day/day4.py
@@ -0,0 +1,20 @@
+def part_1():
+    print('-------------PART I------------')
+    pass
+
+
+def part_2():
+    print('-------------PART II-----------')
+    pass
+
+
+def get_data(filename,separator):
+    with open(filename) as file:
+        return file.readline().rstirp().split(separator)
+
+
+# Main Starts here
+
+print("AoC 2019 Day 2 - Python")
+print("Solution: {}".format(part_1()))
+print("Solution: {}".format(part_2()))
